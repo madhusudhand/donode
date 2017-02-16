f4953ee0ed54c3291009118ced041cb8a7804df7
madhusudhand <madhusudhan.dollu@gmail.com>
2017-02-16 22:30:14 +0530

    adding feature to set response headers

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 const Router = require('./lib/router');
 const Server = require('./lib/server');
-
+const Headers = require('./lib/headers');
 const Controller = require('./lib/base_classes/Controller');
 const Middleware = require('./lib/base_classes/Middleware');
 
 module.exports = {
   Router,
   Server,
+  Headers,
 
   // base classes
   Controller,
